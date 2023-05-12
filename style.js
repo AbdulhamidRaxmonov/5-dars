4026defd22be1d59ac0da59b1dc94e2aeea9afbc
AbdulhamidRaxmonov <itedukokand@gmail.com>
2023-05-12 11:21:08 +0500

    yangilandi

---
+++ style.js
@@ -0,0 +1,11 @@
+var inp1 = document.getElementById("inp1")
+var h1 = document.getElementById("h1")
+function yarat1(){
+    h1.innerText = inp1.value;
+}
+var  h2 = document.getElementById("h2")
+var btn = document.getElementById("btn")
+
+function uzgar(){
+ h2.style.color = "green";
+}
